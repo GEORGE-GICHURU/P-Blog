a538d52538a66fd267caa93e76e6d761b5adeed4
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-05-15 19:42:14 +0300

    committing p-blog

---
+++ config.py
@@ -0,0 +1,60 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'the C blog'
+    SENDER_EMAIL = 'gichurugeorge092@gmail.com'
+
+class ProdConfig(Config):
+    '''
+    Pruduction configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings 
+    '''
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://george:gichuru1@localhost/personal_bog_test'
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://george:gichuru1@localhost/personal_bog'
+
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
